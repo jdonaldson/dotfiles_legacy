ef701db26ddc245b4f6d7e1cef8cdecdd96409c6
Justin Donaldson <jdonaldson@gmail.com>
2017-11-13 10:31:54 -0800

    persistent iterm2 config

---
+++ bootstrap/config_iterm2.sh
@@ -0,0 +1,4 @@
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iterm2/"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
